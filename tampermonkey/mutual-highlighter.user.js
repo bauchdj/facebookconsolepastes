456c0c64ed36921b6651643d22b607d0c32c2e9c
Preston Corless <pgattic@gmail.com>
2021-07-14 14:46:57 -0600

    Create mutual-highlighter.user.js

---
+++ tampermonkey/mutual-highlighter.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Mutual Friends Highlighter
+// @namespace    https://pgattic.github.io
+// @version      1
+// @description  Press the minus ("-") key to cancel the bottom friend request loaded in the list after clicking "View Sent Requests"
+// @author       pgattic, bauchdj
+// @match        https://www.facebook.com/*/friends_current_city
+// @icon         https://static.xx.fbcdn.net/rsrc.php/yD/r/d4ZIVX-5C-b.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var h=prompt("Mutual Friends Threshold?")||Infinity;
+    setInterval(()=>{for(var i of Array.from(document.querySelectorAll('[class="'+document.querySelector('[role="banner"]').firstChild.firstChild.firstChild.classList.value+'"]')).filter(out=>out.innerText.contains("mutual friends"))){var t=i.innerText;if(Number(t.slice(0,t.length-14))>=h){i.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.background="#7b7"}}},300);
+})();
